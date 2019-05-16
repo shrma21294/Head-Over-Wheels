f0b8a5020984ca8b77ad0c7b52c041b83c541b35
komal <sharm314@pnw.edu>
2019-05-16 13:56:37 -0500

    Added first phase of head tracking

---
+++ Assets/PivotPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotPoint : MonoBehaviour {
+
+	public GameObject referenceObject;
+
+	Vector3 offset= new Vector3(0,0,0);
+	
+	// Update is called once per frame
+	void Update () {
+		referenceObject.transform.position = transform.position + offset;
+		referenceObject.transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
+
+		if(transform.localEulerAngles.z>65f){
+			offset += referenceObject.transform.right * 0.1f;
+	}
+
+		if(transform.localEulerAngles.z<-45f){
+			offset += referenceObject.transform.right * -0.1f;
+		}
+}
+}
